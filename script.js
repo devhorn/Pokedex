36814d13d8e89ad204ebac4a7220fab691a6f3af
devhorn <mdevhorn@gmail.com>
2025-01-22 17:40:29 +0100

    started witb overlay

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=1302&offset=0";
-let amountOfRendertPokemons = 12;
-let amountOfLoad = 12;
+let amountOfRendertPokemons = 20;
+let amountOfLoad = 20;
 let loadedPokemons = [];
 let pokemonData = [];
 
 async function init() {
-  await loadPokemons();
-  await getPokemondata();
-  renderPokemons();
+  await initialLoadPokemons();
+  await getInitialPokemonData();
+  initialRenderingOfPokemons();
 }
 
-async function loadPokemons() {
+async function initialLoadPokemons() {
   let pokemonContentRef = document.getElementById("pokemenContent");
   try {
     let response = await fetch(BASE_URL);
     let responseAsJson = await response.json();
     loadedPokemons = responseAsJson.results;
-    console.log(loadedPokemons);
   } catch (error) {
     pokemonContentRef.innerHTML = getLoadDataErrorMessage(error);
   }
 }
 
-async function getPokemondata() {
+async function getInitialPokemonData() {
   let pokemonContentRef = document.getElementById("pokemenContent");
   for (let i = 0; i < amountOfRendertPokemons; i++) {
     try {
@@ -35,7 +34,7 @@ async function getPokemondata() {
   }
 }
 
-function renderPokemons() {
+function initialRenderingOfPokemons() {
   let pokemonContentRef = document.getElementById("pokemenContent");
   for (let pokeIndex = 0; pokeIndex < pokemonData.length; pokeIndex++) {
     pokemonContentRef.innerHTML += getPokemonCardTemplate(pokeIndex);
@@ -43,6 +42,12 @@ function renderPokemons() {
 }
 
 async function loadMorePokemons() {
+  await getMorePokemonData();
+  renderNextPokemons();
+  amountOfRendertPokemons += amountOfLoad;
+}
+
+async function getMorePokemonData() {
   let currentIndex = amountOfRendertPokemons;
   let pokemonContentRef = document.getElementById("pokemenContent");
   for (let i = currentIndex; i < amountOfRendertPokemons + amountOfLoad; i++) {
@@ -54,8 +59,6 @@ async function loadMorePokemons() {
       pokemonContentRef.innerHTML = getLoadDataErrorMessage(error);
     }
   }
-  renderNextPokemons();
-  amountOfRendertPokemons += amountOfLoad;
 }
 
 function renderNextPokemons() {
