71a4168695a3de71fae79e6f469a4be4a1afb501
devhorn <mdevhorn@gmail.com>
2025-01-23 17:47:31 +0100

    startet with detail card

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=1302&offset=0";
-let amountOfRendertPokemons = 8;
-let amountOfLoad = 8;
+let amountOfRendertPokemons = 20;
+let amountOfLoad = 20;
 
 let loadedPokemons = [];
 let pokemonData = [];
@@ -43,57 +43,6 @@ function initialRenderingOfPokemons() {
       pokeIndex,
       pokemonData
     );
-  }
-}
-
-async function loadMorePokemons() {
-  await getMorePokemonData();
-  renderMorePokemons();
-  renderMoreTypes();
-  amountOfRendertPokemons += amountOfLoad;
-}
-
-async function getMorePokemonData() {
-  let currentIndex = amountOfRendertPokemons;
-  let pokemonContentRef = document.getElementById("pokemonContent");
-  for (let i = currentIndex; i < amountOfRendertPokemons + amountOfLoad; i++) {
-    try {
-      let response = await fetch(loadedPokemons[i].url);
-      let responseAsJson = await response.json();
-      pokemonData.push(responseAsJson);
-    } catch (error) {
-      pokemonContentRef.innerHTML = getLoadDataErrorMessage(error);
-    }
-  }
-}
-
-function renderMorePokemons() {
-  let startIndex = amountOfRendertPokemons;
-  let pokemonContentRef = document.getElementById("pokemonContent");
-  for (
-    let pokeIndex = startIndex;
-    pokeIndex < pokemonData.length;
-    pokeIndex++
-  ) {
-    pokemonContentRef.innerHTML += getPokemonCardTemplate(
-      pokeIndex,
-      pokemonData
-    );
-  }
-}
-
-function renderMoreTypes() {
-  let startIndex = amountOfRendertPokemons;
-  for (
-    let pokeIndex = startIndex;
-    pokeIndex < pokemonData.length;
-    pokeIndex++
-  ) {
-    let typeContentRef = document.getElementById(`typeContent${pokeIndex}`);
-    for (let i = 0; i < pokemonData[pokeIndex].types.length; i++) {
-      let typeName = pokemonData[pokeIndex].types[i].type.name;
-      typeContentRef.innerHTML += getTypeTemplate(typeName);
-    }
   }
 }
 
---
+++ scripts/loadMoreFunction.js
@@ -0,0 +1,50 @@
+async function loadMorePokemons() {
+  await getMorePokemonData();
+  renderMorePokemons();
+  renderMoreTypes();
+  amountOfRendertPokemons += amountOfLoad;
+}
+
+async function getMorePokemonData() {
+  let currentIndex = amountOfRendertPokemons;
+  let pokemonContentRef = document.getElementById("pokemonContent");
+  for (let i = currentIndex; i < amountOfRendertPokemons + amountOfLoad; i++) {
+    try {
+      let response = await fetch(loadedPokemons[i].url);
+      let responseAsJson = await response.json();
+      pokemonData.push(responseAsJson);
+    } catch (error) {
+      pokemonContentRef.innerHTML = getLoadDataErrorMessage(error);
+    }
+  }
+}
+
+function renderMorePokemons() {
+  let startIndex = amountOfRendertPokemons;
+  let pokemonContentRef = document.getElementById("pokemonContent");
+  for (
+    let pokeIndex = startIndex;
+    pokeIndex < pokemonData.length;
+    pokeIndex++
+  ) {
+    pokemonContentRef.innerHTML += getPokemonCardTemplate(
+      pokeIndex,
+      pokemonData
+    );
+  }
+}
+
+function renderMoreTypes() {
+  let startIndex = amountOfRendertPokemons;
+  for (
+    let pokeIndex = startIndex;
+    pokeIndex < pokemonData.length;
+    pokeIndex++
+  ) {
+    let typeContentRef = document.getElementById(`typeContent${pokeIndex}`);
+    for (let i = 0; i < pokemonData[pokeIndex].types.length; i++) {
+      let typeName = pokemonData[pokeIndex].types[i].type.name;
+      typeContentRef.innerHTML += getTypeTemplate(typeName);
+    }
+  }
+}
